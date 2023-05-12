77df2af5cc0ba35f98cd670c08ac2adda89c8d67
MOHD SAZID KHAN <sajidpahat786@gmail.com>
2023-05-12 14:36:30 +0530

    Vercel Changes

--- server.js
+++ server.js
@@ -60,6 +60,18 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 4000
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+          if(err) {
+            res.status(500).send(err)
+          }
+      });
+  })
+}
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL)
